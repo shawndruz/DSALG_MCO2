14b7a24fd45710f1bfdacb73158136e901d12cc5
shawndruz <165754470+shawndruz@users.noreply.github.com>
2024-07-27 23:22:29 +0800

    Update structs.h

--- structs.h
+++ structs.h
@@ -1,19 +1,31 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
 // Define the structure for a node in the adjacency list
 typedef struct Node {
-    int dest;
+    int id;
     struct Node* next;
 } Node;
 
-// Define the structure for the adjacency list
+// Structure to represent an adjacency list
 typedef struct AdjList {
     Node* head;
 } AdjList;
 
-// Define the structure for the graph
+// Structure to represent a graph
 typedef struct Graph {
     int numVertices;
     AdjList* array;
 } Graph;
+
+// Function prototypes
+Node* createNode(int id);
+Graph* createGraph(int numVertices);
+void addEdge(Graph* graph, int src, int dest);
+Graph* loadGraphFromFile(char* filename);
+void printGraph(Graph* graph);
+
+#endif // STRUCTS_H
